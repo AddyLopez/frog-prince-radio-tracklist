65d42a5abc81c0e71b8f4aa4c6f7aed9542d773e
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2023-09-07 19:32:04 -0600

    Create table head and populate column headers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { artists } from "./artists-data.js";
 
-const generateTableHead = (table) => {
-  const tableHead = table.createTHead();
+const columnHeaders = ["#", "song", "artist", "album", "duration", "released"];
+
+const createTableHead = (table, data) => {
+  const tableHead = table.createTHead(); // specific method of HTMLTableElement, preferable to generic createElement method
+  const row = tableHead.insertRow(); // specific method of HTMLTableElement
+  for (let columnHeader of columnHeaders) {
+    const th = document.createElement("th");
+    const text = document.createTextNode(columnHeader);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
 };
 
 const table = document.getElementById("table");
-generateTableHead(table);
+createTableHead(table, columnHeaders);
 
 console.log(artists);
-
-/* Full title of Fiona Apple's "When the Pawn...": "When the Pawn Hits the Conflicts He Thinks Like a King What He Knows Throws the Blows When He Goes to the Fight and He'll Win the Whole Thing 'fore He Enters the Ring There's No Body to Batter When Your Mind Is Your Might So  When You Go Solo, You Hold Your Own Hand and Remember that Depth  Is The Greatest of Heights and If You Know Where You Stand, Then You Know Where to Land and If You Fall It Won't Matter, Cuz You'll Know that You're Right" */
